4fd20137d51b1dab0e9bbe4d4d4c06abe565a42d
infinite789 <45011522+infinite789@users.noreply.github.com>
2021-10-05 15:23:46 +0200

    Add files via upload

--- clock.js
+++ clock.js
@@ -27,17 +27,17 @@ function drawFace(ctx, radius) {
 function drawNumbers(ctx, radius) {
   var ang;
   var num;
-  ctx.font = radius * 0.2 + "px arial";
+  ctx.font = radius * 0.15 + "px Verdana";
   ctx.textBaseline = "middle";
   ctx.textAlign = "center";
   for(num = 1; num < 13; num++){
     ang = num * Math.PI / 6;
     ctx.rotate(ang);
-    ctx.translate(0, -radius * 0.85);
+    ctx.translate(0, -radius * 0.8);
     ctx.rotate(-ang);
     ctx.fillText(num.toString(), 0, 0);
     ctx.rotate(ang);
-    ctx.translate(0, radius * 0.85);
+    ctx.translate(0, radius * 0.8);
     ctx.rotate(-ang);
   }
 }
